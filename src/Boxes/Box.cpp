ddf093333a4b94255f6f3d2c56032812787759ae
connor.gallimore@epitech.eu <cgallimore@localhost.localdomain>
2018-05-16 18:53:22 +0200

    Changed box to work with IObject

--- src/Boxes/Box.cpp
+++ src/Boxes/Box.cpp
@@ -6,13 +6,14 @@
 
 /**
  * Constructor : Ready To use. Create a box with the loot appropriate (20% luck)
- * @param posx 
- * @param posy 
+ * @param posx
+ * @param posy
  * @param loot : if theres is no loot it is set to nullptr
  */
-Box::Box(int posx, int posy, Loot loot) : _posx{posx}, _posy{posy}, _loot{loot}, _pathToMesh{"PATH BOXE"}, _destroy{false}
+Box::Box(Loot loot)
+	: _loot{loot}, _pathToMesh{"PATH BOXE"}, _destroy{false}
 {
-
+	setType(BOX);
 }
 
 Box::~Box()
--- src/Boxes/Box.hpp
+++ src/Boxes/Box.hpp
@@ -5,21 +5,20 @@
 #pragma once
 
 #include <string>
-#include "Loot.hpp"
+#include "../Map/IObject.hpp"
+#include "../Loot/Loot.hpp"
 
-class Box {
+class Box : public map::IObject {
 	public:
-		Box(int posx, int posy, Loot loot);
+		Box(Loot loot);
 		~Box();
 
 		void destroyBox() noexcept { _destroy = true; };
-		
+
 		//Getter
 		bool isDestroyed() const noexcept { return _destroy; };
 
 	private:
-		int _posx;
-		int _posy;
 		Loot _loot;
 		std::string _pathToMesh;
 		bool _destroy;
