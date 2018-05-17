fa4c0bc9a4c404cd8e2f7574e78185be0cf1787d
Connor Gallimore <connor.gallimore@epitech.eu>
2018-05-18 00:19:16 +0200

    Implemented

--- src/Object/Fire/Fire.cpp
+++ src/Object/Fire/Fire.cpp
@@ -6,7 +6,32 @@
 
 namespace object {
 
-Fire::Fire() : AObject("BOMB PATH", FIRE)
-{}
+Fire::Fire(size_t posx, size_t posy, map::Map *map)
+	: AObject("BOMB PATH", FIRE),
+	_posx{posx},
+	_posy{posy},
+	_map{map},
+	_dissipated{false}
+{
+	_begin = std::clock();
+}
+
+void Fire::update() noexcept
+{
+	if (((std::clock() - _begin) / (double) CLOCKS_PER_SEC) >= LIFE) {
+		dissipate();
+		return;
+	}
+}
+
+void Fire::dissipate() noexcept
+{
+	if (_dissipated)
+		return;
+
+	_dissipated = true;
+
+	_map->removeObjectFromTile(_posx, _posy, FIRE);
+}
 
 }
--- src/Object/Fire/Fire.hpp
+++ src/Object/Fire/Fire.hpp
@@ -4,14 +4,30 @@
 
 #pragma once
 
+#include <ctime>
 #include "AObject.hpp"
+#include "Map.hpp"
 
 namespace object {
 
 class Fire : public object::AObject {
+
+	#define LIFE 0.4
+
 	public:
-		Fire();
+		Fire(size_t posx, size_t posy, map::Map *map);
 		~Fire() = default;
+
+		void update() noexcept;
+
+	private:
+		void dissipate() noexcept;
+
+		size_t _posx;
+		size_t _posy;
+		map::Map *_map;
+		bool _dissipated;
+		std::clock_t _begin;
 };
 
 }
