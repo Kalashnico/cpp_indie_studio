3cda77ccf733007d674c03e09095b0fb86ac2337
Connor Gallimore <connor.gallimore@epitech.eu>
2018-06-04 12:59:46 +0200

    Updated main for new collisions

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,8 @@
 
 #include "AI.hpp"
 #include "Irrlicht/irrlicht.h"
-#include "Graphic/CustomEventReceiver.hpp"
-#include "Graphic/Gfx.hpp"
-#include "Graphic/CollisionsHandler.hpp"
+#include "CustomEventReceiver.hpp"
+#include "Gfx.hpp"
 
 using namespace irr;
 
@@ -19,25 +18,21 @@ int main()
 {
 	CustomEventReceiver eventReceiver;
 	Gfx gfx(eventReceiver);
-	CollisionsHandler collisionsHandler(gfx);
-	::map::Map map(&gfx, &collisionsHandler);
+	::map::Map map(&gfx);
 	vector3df pos = {0.f, 0.f, 0.f};
 	pos -= 6.f;
 	pos *= 4.f;
 	pos.Y = 0.1f;
-	auto player = std::make_unique<object::Player>(&map, "", PLAYER1, &gfx, POMMY, pos.X, pos.Y, pos.Z, false, 1);
+	auto player = std::make_unique<object::Player>(&map, PLAYER1, &gfx, POMMY, pos.X, pos.Y, pos.Z, false, 1);
 	vector3df pos2 = {0.f, 0.f, MAP_SIZE - 1.f};
 	pos2 -= 6.f;
 	pos2 *= 4.f;
 	pos2.Y = 0.1f;
-	auto player2 = std::make_unique<object::AI>(&map, "", PLAYER2, &gfx, POMMY, pos2.X, pos2.Y, pos2.Z, false, 2);
+	auto player2 = std::make_unique<object::AI>(&map, PLAYER2, &gfx, POMMY, pos2.X, pos2.Y, pos2.Z, false, 2);
 	auto tmp = player.get()->getPosition();
 	auto tmp2 = player2.get()->getPlayer().getPosition();
 
 	try {
-		collisionsHandler.addObjectToCollisions(*player.get());
-		collisionsHandler.addObjectToCollisions(player2.get()->getPlayer());
-		collisionsHandler.addMapToCollision(map);
 		gfx.addCameraFPS();
 		gfx.addLight(vector3df(-30, 30, -30), SColorf(1.0f, 1.0f, 1.0f),
 			20);
