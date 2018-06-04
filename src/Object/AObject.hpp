dc6112193deea344f9a18ce01d587f602f419fba
Connor Gallimore <connor.gallimore@epitech.eu>
2018-06-04 21:35:34 +0200

    Changed AObject  to include getDead and getPosition

--- src/Object/AObject.hpp
+++ src/Object/AObject.hpp
@@ -6,6 +6,7 @@
 
 #include <string>
 #include <memory>
+#include "Irrlicht/irrlicht.h"
 
 enum Type {
 	WALL,
@@ -41,12 +42,16 @@ class AObject {
 
 		virtual void resetGfx() noexcept {}
 
+		virtual bool isDead() noexcept { return false; }
+
 		virtual void detonate() noexcept {}
 		virtual std::unique_ptr<Loot> getLoot() noexcept;
 		virtual LootCategory getLootCategory() const noexcept {}
 
 		virtual void update() noexcept {}
 		virtual void updatePosition(size_t oldx, size_t oldy) noexcept {}
+
+		virtual irr::core::vector2di getPosition() noexcept { return {0, 0}; }
 
 		//Getter
 		Type getType() const noexcept { return _type; }
