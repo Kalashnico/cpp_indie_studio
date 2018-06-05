3c6fe307c049a9eac7a5f19f577df76260f6c91f
Connor Gallimore <connor.gallimore@epitech.eu>
2018-06-05 15:30:37 +0200

    Balance

--- src/Object/Boxes/Loot.cpp
+++ src/Object/Boxes/Loot.cpp
@@ -2,7 +2,6 @@
 // Created by Nicolas Guerin on 14/05/2018.
 //
 
-#include <iostream>
 #include <random>
 #include "Loot.hpp"
 
@@ -11,43 +10,76 @@ namespace object {
 /**
  * Constructor : Ready to use. It create randomly the loot which will be stored in a box.
  */
-Loot::Loot() : AObject(LOOT)
+Loot::Loot(int posx, int posy, Gfx *gfx)
+	: AObject(LOOT), _posx(posx), _posy(posy), _gfx(gfx)
 {
 	setupLoot();
 }
 
 Loot::~Loot()
-{}
+{
+	_gfx->deleteElement("loot " + std::to_string(_posx) + " " + std::to_string(_posy));
+}
+
+void Loot::display() noexcept
+{
+	irr::core::vector3df pos = {(float)_posy, 0.f, (float)_posx};
+	pos -= 6.f;
+	pos *= 4.f;
+	pos.Y = 1.5f;
+	_node = _gfx->drawMesh(_modelPath, "", false, "loot " + std::to_string(_posx) + " " + std::to_string(_posy), pos);
+	_node->setScale(_modelSize);
+}
 
 void Loot::setupLoot() noexcept
 {
 	std::random_device randomDevice;			// Random device
 	std::mt19937 engine(randomDevice());			// Seed
-	std::uniform_int_distribution<> distribution(1, 3);	// Range
+	std::uniform_int_distribution<> distribution(1, 5);	// Range
 
-	if (distribution(engine) < 3) {
+	if (distribution(engine) < 5) {
 		_category = EMPTY;
 		return;
 	}
 
-	auto lootCategory = std::rand() % 4;
+	auto lootCategory = std::rand() % 13;
+
+	if (lootCategory < 4)
+		lootCategory = 0;
+	else if (lootCategory < 8)
+		lootCategory = 1;
+	else if (lootCategory < 12)
+		lootCategory = 2;
+	else if (lootCategory == 12)
+		lootCategory = 3;
+
+	lootCategory = 2;
 
 	switch (lootCategory) {
 		case 0:
 			_category = BOMB_UP;
+			_modelPath = "./media/models/bomb/bomb.obj";
+			_modelSize = {0.25f, 0.25f, 0.25f};
 			break;
 		case 1:
 			_category = FIRE_UP;
+			_modelPath = "./media/models/fire/WpnMetaridleyFireballS.obj";
+			_modelSize = {0.15f, 0.15f, 0.15f};
 			break;
 		case 2:
 			_category = SPEED_UP;
+			_modelPath = "./media/models/speed/Hover Boots.obj";
+			_modelSize = {5.f, 5.f, 5.f};
 			break;
 		case 3:
 			_category = WALL_PASS;
+			_modelPath = "./media/models/box/ItmBox00.obj";
+			_modelSize = {0.15f, 0.15f, 0.15f};
 			break;
 		default :
 			_category = EMPTY;
 			break;
 	}
 }
+
 }
--- src/Object/Boxes/Loot.hpp
+++ src/Object/Boxes/Loot.hpp
@@ -6,21 +6,32 @@
 
 #include <string>
 #include <array>
+#include "Irrlicht/irrlicht.h"
 #include "AObject.hpp"
+#include "Gfx.hpp"
 
 namespace object {
 
 //Theses are the paths to loot meshs
 class Loot : public AObject {
 	public:
-		Loot();
+		Loot(int posx, int posy, Gfx *gfx);
 		~Loot();
+
+		void display() noexcept;
 
 		//Getters
 		LootCategory getLootCategory() const noexcept { return _category; };
 
 	private:
 		LootCategory _category;
+		std::string _modelPath;
+		irr::core::vector3df _modelSize;
+
+		int _posx;
+		int _posy;
+		Gfx *_gfx;
+		irr::scene::IAnimatedMeshSceneNode *_node;
 
 		//Member Function
 		void setupLoot() noexcept;
