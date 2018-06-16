1b05aee5f43d78061372b50be2148f6ab07eea49
Connor Gallimore <connor.gallimore@epitech.eu>
2018-06-16 18:29:14 +0200

    Main menu added

--- src/Core/Core.cpp
+++ src/Core/Core.cpp
@@ -8,11 +8,116 @@
 
 namespace core {
 
-Core::Core(int numPlayers)
+Core::Core()
 {
+	sound::SoundManager::getInstance().playBackground();
 
+	_numPlayers = 1;
+	_playerWon = -1;
+	_playerWonTextSetup = true;
 	_gfx.setEventReceiver(_eventReceiver);
-	_map.setGfx(&_gfx);
+
+	try {
+		_gfx.addCameraFPS();
+		_gfx.addLight(irr::core::vector3df(-30, 30, -30), irr::video::SColorf(1.0f, 1.0f, 1.0f), 20);
+		_gfx.addLight(irr::core::vector3df(-30, 30, 30), irr::video::SColorf(1.0f, 1.0f, 1.0f), 20);
+		_gfx.addLight(irr::core::vector3df(30, 30, -30), irr::video::SColorf(1.0f, 1.0f, 1.0f), 20);
+		_gfx.addLight(irr::core::vector3df(30, 30, 30), irr::video::SColorf(1.0f, 1.0f, 1.0f), 20);
+	} catch (...) {
+		std::cerr << "Something went wrong" << std::endl;
+		return;
+	}
+}
+
+Core::~Core()
+{}
+
+void Core::run() noexcept
+{
+	menu();
+}
+
+void Core::menu() noexcept
+{
+	_gfx.getDevice()->getCursorControl()->setVisible(true);
+
+	_env = _gfx.getDevice()->getGUIEnvironment();
+	_env->getSkin()->setFont(_env->getFont("./media/fonts/OpenSans-Bold.png"));
+
+	_elements.emplace_back(_env->addImage(_gfx.getDriver()->getTexture("./media/menu/title.png"), irr::core::position2d<int>(WIN_WIDTH / 6.5f, 10)));
+	_elements.emplace_back(_env->addButton(irr::core::rect<irr::s32>(WIN_WIDTH / 2 - 52, 340, WIN_WIDTH / 2 + 52, 340 + 60), 0, PLAY_BUTTON, L"Play", L""));
+	_elements.emplace_back(_env->addButton(irr::core::rect<irr::s32>(WIN_WIDTH / 2 - 52, 420, WIN_WIDTH / 2 + 52, 420 + 60), 0, QUIT_BUTTON, L"Quit", L""));
+	_elements.emplace_back(_env->addButton(irr::core::rect<irr::s32>(WIN_WIDTH / 2 + 52, 580, WIN_WIDTH / 2 + 104, 580 + 60), 0, ADD_PLAYER_BUTTON, L">", L""));
+	_elements.emplace_back(_env->addButton(irr::core::rect<irr::s32>(WIN_WIDTH / 2 - 104, 580, WIN_WIDTH / 2 - 52, 580 + 60), 0, REMOVE_PLAYER_BUTTON, L"<", L""));
+
+	irr::core::stringw numPlayers = L"Number of players: ";
+	numPlayers += _numPlayers;
+
+	_elements.emplace_back(_env->addStaticText(numPlayers.c_str(), irr::core::rect<irr::s32>(WIN_WIDTH / 2 - 220, 500, WIN_WIDTH / 2 + 220, 500 + 60), false, false, 0, -1, false));
+	auto playerNumElement = _elements.back();
+
+	irr::core::stringw lastWinner = L"Player ";
+	lastWinner += _playerWon;
+	lastWinner += L" won!";
+
+	if (_playerWon == 0)
+		lastWinner = L"        Draw!";
+
+	_elements.emplace_back(_env->addStaticText(lastWinner.c_str(), irr::core::rect<irr::s32>(WIN_WIDTH / 2 - 135, 660, WIN_WIDTH / 2 + 135, 660 + 60), false, false, 0, -1, false));
+	auto playerWonElement = _elements.back();
+
+	if (_playerWon == -1 && playerWonElement->isVisible())
+		playerWonElement->setVisible(false);
+
+	while (_gfx.isRunning()) {
+		if (_gfx.isGuiButtonPressed(QUIT_BUTTON))
+			return;
+
+		if (_gfx.isGuiButtonPressed(PLAY_BUTTON))
+			game();
+
+		if (_gfx.isGuiButtonPressed(ADD_PLAYER_BUTTON)) {
+			_numPlayers++;
+			if (_numPlayers > 4)
+				_numPlayers = 4;
+		}
+
+		if (_gfx.isGuiButtonPressed(REMOVE_PLAYER_BUTTON)) {
+			_numPlayers--;
+			if (_numPlayers < 0)
+				_numPlayers = 0;
+		}
+
+		numPlayers = L"Number of players: ";
+		numPlayers += _numPlayers;
+		playerNumElement->setText(numPlayers.c_str());
+
+		if (!_playerWonTextSetup) {
+			irr::core::stringw lastWinner = L"Player ";
+			lastWinner += _playerWon;
+			lastWinner += L" won!";
+
+			if (_playerWon == 0)
+				lastWinner = L"        Draw!";
+
+			playerWonElement->setText(lastWinner.c_str());
+
+			if (_playerWon == -1)
+				playerWonElement->setVisible(false);
+
+			_playerWonTextSetup = true;
+		}
+
+		_gfx.update();
+	}
+}
+
+void Core::game() noexcept
+{
+	for (auto _element : _elements)
+		_element->setVisible(false);
+
+	auto _map = std::make_unique<map::Map>(&_gfx);
 
 	irr::core::vector3df tilePos1 = {0.f, 0.f, 0.f};
 	irr::core::vector3df tilePos2 = {0.f, 0.f, MAP_SIZE - 1.f};
@@ -28,55 +133,46 @@ Core::Core(int numPlayers)
 	std::unique_ptr<object::AObject> player3;
 	std::unique_ptr<object::AObject> player4;
 
-	(numPlayers > 0) ? player1 = std::make_unique<object::Player>(&_map, PLAYER1, &_gfx, POMMY, iPos1.X, iPos1.Y, iPos1.Z, false, 1)
-			: player1 = std::make_unique<object::AI>(&_map, PLAYER1, &_gfx, POMMY, iPos1.X, iPos1.Y, iPos1.Z, false, 1);
+	auto numPlayers = _numPlayers;
+
+	(numPlayers > 0) ? player1 = std::make_unique<object::Player>(_map.get(), PLAYER1, &_gfx, POMMY, iPos1.X, iPos1.Y, iPos1.Z, false, 1)
+			: player1 = std::make_unique<object::AI>(_map.get(), PLAYER1, &_gfx, POMMY, iPos1.X, iPos1.Y, iPos1.Z, false, 1);
 
 	numPlayers--;
 
-	(numPlayers > 0) ? player2 = std::make_unique<object::Player>(&_map, PLAYER2, &_gfx, POMMY, iPos2.X, iPos2.Y, iPos2.Z, false, 2)
-			: player2 = std::make_unique<object::AI>(&_map, PLAYER2, &_gfx, POMMY, iPos2.X, iPos2.Y, iPos2.Z, false, 2);
+	(numPlayers > 0) ? player2 = std::make_unique<object::Player>(_map.get(), PLAYER2, &_gfx, POMMY, iPos2.X, iPos2.Y, iPos2.Z, false, 2)
+			: player2 = std::make_unique<object::AI>(_map.get(), PLAYER2, &_gfx, POMMY, iPos2.X, iPos2.Y, iPos2.Z, false, 2);
 
 	numPlayers--;
 
-	(numPlayers > 0) ? player3 = std::make_unique<object::Player>(&_map, PLAYER3, &_gfx, POMMY, iPos3.X, iPos3.Y, iPos3.Z, false, 3)
-			: player3 = std::make_unique<object::AI>(&_map, PLAYER3, &_gfx, POMMY, iPos3.X, iPos3.Y, iPos3.Z, false, 3);
+	(numPlayers > 0) ? player3 = std::make_unique<object::Player>(_map.get(), PLAYER3, &_gfx, POMMY, iPos3.X, iPos3.Y, iPos3.Z, false, 3)
+			: player3 = std::make_unique<object::AI>(_map.get(), PLAYER3, &_gfx, POMMY, iPos3.X, iPos3.Y, iPos3.Z, false, 3);
 
 	numPlayers--;
 
-	(numPlayers > 0) ? player4 = std::make_unique<object::Player>(&_map, PLAYER4, &_gfx, POMMY, iPos4.X, iPos4.Y, iPos4.Z, false, 4)
-			: player4 = std::make_unique<object::AI>(&_map, PLAYER4, &_gfx, POMMY, iPos4.X, iPos4.Y, iPos4.Z, false, 4);
+	(numPlayers > 0) ? player4 = std::make_unique<object::Player>(_map.get(), PLAYER4, &_gfx, POMMY, iPos4.X, iPos4.Y, iPos4.Z, false, 4)
+			: player4 = std::make_unique<object::AI>(_map.get(), PLAYER4, &_gfx, POMMY, iPos4.X, iPos4.Y, iPos4.Z, false, 4);
 
-	try {
-		_gfx.addCameraFPS();
-		_gfx.addLight(irr::core::vector3df(-30, 30, -30), irr::video::SColorf(1.0f, 1.0f, 1.0f), 20);
-		_gfx.addLight(irr::core::vector3df(-30, 30, 30), irr::video::SColorf(1.0f, 1.0f, 1.0f), 20);
-		_gfx.addLight(irr::core::vector3df(30, 30, -30), irr::video::SColorf(1.0f, 1.0f, 1.0f), 20);
-		_gfx.addLight(irr::core::vector3df(30, 30, 30), irr::video::SColorf(1.0f, 1.0f, 1.0f), 20);
-	} catch (...) {
-		std::cerr << "Something went wrong" << std::endl;
-		return;
-	}
 
-	_map.addObjectToTile(static_cast<size_t>(tilePos1.X), static_cast<size_t>(tilePos1.Y), std::move(player1));
-	_map.addObjectToTile(static_cast<size_t>(tilePos2.X), static_cast<size_t>(tilePos2.Y), std::move(player2));
-	_map.addObjectToTile(static_cast<size_t>(tilePos3.X), static_cast<size_t>(tilePos3.Y), std::move(player3));
-	_map.addObjectToTile(static_cast<size_t>(tilePos4.X), static_cast<size_t>(tilePos4.Y), std::move(player4));
-}
+	_map.get()->addObjectToTile(static_cast<size_t>(tilePos1.X), static_cast<size_t>(tilePos1.Y), std::move(player1));
+	_map.get()->addObjectToTile(static_cast<size_t>(tilePos2.X), static_cast<size_t>(tilePos2.Y), std::move(player2));
+	_map.get()->addObjectToTile(static_cast<size_t>(tilePos3.X), static_cast<size_t>(tilePos3.Y), std::move(player3));
+	_map.get()->addObjectToTile(static_cast<size_t>(tilePos4.X), static_cast<size_t>(tilePos4.Y), std::move(player4));
 
-Core::~Core()
-{}
-
-void Core::run() noexcept
-{
-
-	sound::SoundManager::getInstance().playBackground();
+	_gfx.getDevice()->getCursorControl()->setVisible(false);
 
 	_begin = std::clock();
 
-	while (_gfx.isRunning() && !_map.shouldEndGame()) {
+	while (_gfx.isRunning() && !_map.get()->shouldEndGame()) {
 
-		if (_gfx.isKeyDown(irr::KEY_ESCAPE))
+		if (_gfx.isKeyDown(irr::KEY_ESCAPE)) {
+			_playerWon = -1;
+			_playerWonTextSetup = false;
+			_gfx.getDevice()->getCursorControl()->setVisible(true);
+			for (auto _element : _elements)
+				_element->setVisible(true);
 			return;
+		}
 
 		if (((std::clock() - _begin) / (double) CLOCKS_PER_SEC) < (1.0 / 60.0))
 			continue;
@@ -84,8 +180,17 @@ void Core::run() noexcept
 		_begin = std::clock();
 
 		_gfx.update();
-		_map.updateTileObjects();
+		_map.get()->updateTileObjects();
 	}
+
+	_playerWon = _map.get()->getWinner();
+
+	_playerWonTextSetup = false;
+
+	_gfx.getDevice()->getCursorControl()->setVisible(true);
+
+	for (auto _element : _elements)
+		_element->setVisible(true);
 }
 
 irr::core::vector3df Core::tileToIrrlicht(irr::core::vector3df tilePos) noexcept
--- src/Core/Core.hpp
+++ src/Core/Core.hpp
@@ -8,23 +8,39 @@
 #include "Map.hpp"
 #include "AI.hpp"
 
+
 namespace core {
+
+enum guiElements {
+	TITLE = 100,
+	PLAY_BUTTON = 101,
+	QUIT_BUTTON = 102,
+	ADD_PLAYER_BUTTON = 103,
+	REMOVE_PLAYER_BUTTON = 104
+};
 
 class Core {
 	public:
-		Core(int numPlayers);
+		Core();
 		~Core();
 
 		void run() noexcept;
 
 	private:
+		int _numPlayers;
+		int _playerWon;
+		bool _playerWonTextSetup;
+
+		std::vector<irr::gui::IGUIElement *> _elements;
+
 		CustomEventReceiver _eventReceiver;
 		Gfx _gfx;
-		::map::Map _map;
+		irr::gui::IGUIEnvironment *_env;
 
 		std::clock_t _begin;
 
-		bool shouldEndGame() const noexcept;
+		void menu() noexcept;
+		void game() noexcept;
 		irr::core::vector3df tileToIrrlicht(irr::core::vector3df tilePos) noexcept;
 };
 
